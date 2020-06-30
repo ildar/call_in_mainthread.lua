ca298e8be5932d048711d9b66ea0c4afca934612
Ildar Mulyukov <ildar@altlinux.ru>
2020-07-01 00:54:55 +0600

    add returning error test

--- spec/1_spec.lua
+++ spec/1_spec.lua
@@ -3,12 +3,11 @@ local cim = require 'call_in_mainthread'
 describe("This module", function()
   it("can execute a function",
     function()
-      local status, info
+      local status, info, costatus
       local tab = {}
       local function f()
         for i=1,3 do
-          local status = cim.mainthread_call(table.insert, tab, i)
-          assert.is_equal( true, status )
+          costatus = cim.mainthread_call(table.insert, tab, i)
         end
       end
       local co = coroutine.create(f)
@@ -16,7 +15,25 @@ describe("This module", function()
         status, info = coroutine.resume(co)
         cim.mainthread_process()
       until not status
+      assert.is_equal( true, costatus )
       assert.is_equal( "123", table.concat(tab) )
     end)
+
+  it("can deal with errors in functions",
+    function()
+      local status, info, costatus, res
+      local tab = {}
+      local function f()
+        costatus, res = cim.mainthread_call(error, "error msg")
+      end
+      local co = coroutine.create(f)
+      repeat
+        status, info = coroutine.resume(co)
+        cim.mainthread_process()
+      until not status
+      assert.is_equal( false, costatus )
+      assert.is_equal( "error msg", res )
+    end)
+
 end)
   
